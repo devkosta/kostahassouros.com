aefc640b75c2a44791746fb256ecae66e8901975
kostashassouros <kostas.hassouros@gmail.com>
2022-02-12 15:57:23 +1030

    Added essentials Gatsby/Netlify plugins

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
+import FadeIn from "react-fade-in";
+
 import BaseLayout from "../components/Layout/BaseLayout";
 import Section from "../components/Layout/Section";
+import Link from "../components/Link";
 
 import { Text } from "@chakra-ui/react";
 
 const NotFoundPage = () => (
     <BaseLayout>
-        <Section>
-            <Text fontSize="2xl">404! Not much to see here...</Text>
-        </Section>
+		<FadeIn>
+            <Section>
+                <Text fontSize="2xl">404! Not much to see here... Go <Link toHref="/" color="#6666FF">Home!</Link></Text>
+            </Section>
+        </FadeIn>
     </BaseLayout>
 );
 
